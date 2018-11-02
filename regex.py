573c09dc746a842e725910bf9211fd46ce895110
Tedu <tarena@tedu.cn>
2018-11-02 16:17:40 +0800

    add some files

---
+++ regex.py
@@ -0,0 +1,22 @@
+import re
+
+pattern = r"(\w+):(\d+)"
+s = 'zhang:1994 li:1993'
+
+#re直接调用
+# l = re.findall(pattern,s,flags = 0)
+# print(l)
+
+#compile对象调用
+regex = re.compile(pattern,flags = 0)
+l = regex.findall(s, pos = 0, endpos = 12)
+print(l)
+
+l1 = re.split(r'\s+', 'Hello  world')
+print(l1)
+
+s = re.sub(r'\s+', '##', 'Hello  world  nihao beijing',4)
+print(s)
+
+s = re.subn(r'\s+', '##', 'Hello  world  nihao beijing',4)
+print(s)
